8948f3ed422cb0c4757987f3fe2c138273e03f6e
Amanda Nelson <amanda.gaylord@gmail.com>
2020-10-11 19:58:48 -0400

    framework for the carousel elements created.

--- components/Carousel.js
+++ components/Carousel.js
@@ -19,15 +19,41 @@
     <img src="https://tk-assets.lambdaschool.com/0b770382-d0eb-4465-8bf2-692a79fcda71_turntable.jpeg" />
     <div class="right-button"> > </div>
   </div>
-*/
-function carouselComp () {
+  */
+//current html container for the carousel:
+const carouselPlaceholder = document.getElementsByClassName(
+  "carousel-container"
+);
+function carouselComp() {
+  //elements
   const carouselParent = document.createElement("div");
-  carouselParent.classList.add("carousel");
   const leftbutton = document.createElement("div");
-  leftbutton.classList.add("carousel");
   const img1 = document.createElement("img");
-  const img2 = document.createElement("")
-  return carouselParent;
+  const img2 = document.createElement("img");
+  const img3 = document.createElement("img");
+  const img4 = document.createElement("img");
+  const rightbutton = document.createElement("div");
+  //classes
+  carouselParent.classList.add("carousel");
+  leftbutton.classList.add("left-button");
+  rightbutton.classList.add("right-button");
+  //contents
+  img1.src =
+    "https://tk-assets.lambdaschool.com/ba687af4-3a1e-43d7-87b2-f30453264c9d_mountains.jpeg";
+  img2.src =
+    "https://tk-assets.lambdaschool.com/8aa075b0-67cf-47ce-9a7f-8cc9d754675d_computer.jpeg";
+  img3.src =
+    "https://tk-assets.lambdaschool.com/5b7441c6-6e4b-4feb-a4ec-8dd2eb76238a_trees.jpeg";
+  img4.src =
+    "https://tk-assets.lambdaschool.com/0b770382-d0eb-4465-8bf2-692a79fcda71_turntable.jpeg";
+  //putting children with the parent
+  carouselParent.appendChild(leftbutton);
+  carouselParent.appendChild(img1);
+  carouselParent.appendChild(img2);
+  carouselParent.appendChild(img3);
+  carouselParent.appendChild(img4);
+  carouselParent.appendChild(rightbutton);
+  console.log(carouselParent);
+  console.log(carouselPlaceholder);
 }
-const carouselPlaceholder = document.querySelector(".carousel-constainer");
-carouselPlaceholder.appendChild(carouselComp())
+carouselPlaceholder.appendChild(carouselComp());
