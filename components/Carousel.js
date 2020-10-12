3e440c773eb8929eff40fa159e8827484c3328af
Amanda Nelson <amanda.gaylord@gmail.com>
2020-10-11 23:05:00 -0400

    changes made to the carousel.js which will take too much additional time to get it to work. I'll work on selecting article topics in the cards.js next.

--- components/Carousel.js
+++ components/Carousel.js
@@ -20,40 +20,42 @@
     <div class="right-button"> > </div>
   </div>
   */
+//elements
+const carouselParent = document.createElement("div");
+const leftbutton = document.createElement("div");
+const img1 = document.createElement("img");
+const img2 = document.createElement("img");
+const img3 = document.createElement("img");
+const img4 = document.createElement("img");
+const rightbutton = document.createElement("div");
+//classes
+carouselParent.classList.add("carousel");
+leftbutton.classList.add("left-button");
+rightbutton.classList.add("right-button");
+img1.classList.add("active");
+img2.classList.add("inactive");
+img3.classList.add("inactive");
+img4.classList.add("inactive");
+//contents
+img1.src =
+  "https://tk-assets.lambdaschool.com/ba687af4-3a1e-43d7-87b2-f30453264c9d_mountains.jpeg";
+img2.src =
+  "https://tk-assets.lambdaschool.com/8aa075b0-67cf-47ce-9a7f-8cc9d754675d_computer.jpeg";
+img3.src =
+  "https://tk-assets.lambdaschool.com/5b7441c6-6e4b-4feb-a4ec-8dd2eb76238a_trees.jpeg";
+img4.src =
+  "https://tk-assets.lambdaschool.com/0b770382-d0eb-4465-8bf2-692a79fcda71_turntable.jpeg";
+//putting children with the parent
+carouselParent.appendChild(leftbutton);
+carouselParent.appendChild(img1);
+carouselParent.appendChild(img2);
+carouselParent.appendChild(img3);
+carouselParent.appendChild(img4);
+carouselParent.appendChild(rightbutton);
+//then puting it into the HTML space for it:
 //current html container for the carousel:
-const carouselPlaceholder = document.getElementsByClassName(
+console.log(carouselParent);
+document.getElementsByClassName(
   "carousel-container"
-);
-function carouselComp() {
-  //elements
-  const carouselParent = document.createElement("div");
-  const leftbutton = document.createElement("div");
-  const img1 = document.createElement("img");
-  const img2 = document.createElement("img");
-  const img3 = document.createElement("img");
-  const img4 = document.createElement("img");
-  const rightbutton = document.createElement("div");
-  //classes
-  carouselParent.classList.add("carousel");
-  leftbutton.classList.add("left-button");
-  rightbutton.classList.add("right-button");
-  //contents
-  img1.src =
-    "https://tk-assets.lambdaschool.com/ba687af4-3a1e-43d7-87b2-f30453264c9d_mountains.jpeg";
-  img2.src =
-    "https://tk-assets.lambdaschool.com/8aa075b0-67cf-47ce-9a7f-8cc9d754675d_computer.jpeg";
-  img3.src =
-    "https://tk-assets.lambdaschool.com/5b7441c6-6e4b-4feb-a4ec-8dd2eb76238a_trees.jpeg";
-  img4.src =
-    "https://tk-assets.lambdaschool.com/0b770382-d0eb-4465-8bf2-692a79fcda71_turntable.jpeg";
-  //putting children with the parent
-  carouselParent.appendChild(leftbutton);
-  carouselParent.appendChild(img1);
-  carouselParent.appendChild(img2);
-  carouselParent.appendChild(img3);
-  carouselParent.appendChild(img4);
-  carouselParent.appendChild(rightbutton);
-  console.log(carouselParent);
-  console.log(carouselPlaceholder);
-}
-carouselPlaceholder.appendChild(carouselComp());
+).innerHTML += carouselParent;
+//cannot seem to get this to appear on the page... 🤔
